f717fb177b9ed2981a029e3b99cd54a167903d59
MHHuong <147985885+MHHuong@users.noreply.github.com>
2023-10-15 22:55:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Cô Dáo Tón';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100037250806865';
+const eventName = 'Chúc Mừng Sinh Nhựt';
+const titleCard = 'Gửi người bán chuối chiên dốc cầu Cao Lãnh';
+const contentCard = 'Sinh Nhựt dui dẻ, lúc bạn đọc cái này là lúc tui đang lao lực dới đại số tuyến tính r. Đầu tiên thì trình độ của tui nó chưa thượng thừa tới mức cân được nguyên cái web này đâu nên đừng bất ngờ, tui cũng lên gu gồ search hơi nhiều mới được như vầy nên chê thì hận cả đời nhe. Đầu tiên chắc chúc mừng đủ tuổi lấy chồng, sau đó là chia buồn là bạn già thêm 1 tuổi rồi đó (liu liu đồ già). Chúc bạn chủ nhật tuần này pass được anh văn, càng nhiều càng tốt, vậy thôi chứ tui cũng hết biết chúc dì rồi với cũng khuya rồi nên tui đi ngủ đây. Túm cái váy lại là CMSN (chúc mày số nhọ) với nhiều deadline vậy thoi, bai bai.';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
